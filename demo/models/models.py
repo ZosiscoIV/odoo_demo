9475dd391e1e9cbb9bb4fa995a52068b0af1582e
Antoine <antoinebontems05@gmail.com>
2025-11-12 16:15:39 +0100

    [FIX] Ajout d'un Raise dans models.py

--- demo/models/models.py
+++ demo/models/models.py
@@ -6,13 +6,19 @@ class User(models.Model):
     _inherit = 'res.users'
     hobby = fields.Many2one('demo.hobby', string='Favorite Hobby')
     hobbies = fields.Many2many('demo.hobby', string='Hobbies', relation='demo_user_hobby', column1='user_id', column2='hobby_id')
-    description = fields.Char(string='Description', required=True)
+    description = fields.Char(string='Description', required=True, default='No Description')
 
     def create(self, vals):
         if 'description' not in vals:
             vals['description'] = f"I'm {vals['name']}"
         return super(User, self).create(vals)
                                         
+    @api.constrains('description')
+    def _description_is_required(self):
+        for user in self:
+            if not user.description:
+                raise ValueError('Description is required')
+
     @api.constrains('description')
     def _description_is_one_line(self):
         for user in self:
